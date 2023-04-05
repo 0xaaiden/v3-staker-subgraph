c8877090a6bb76383598a75245115b6e54c2c369
0xJiro <jiro@sushi.com>
2023-04-05 02:09:11 -0400

    fix position schema

--- src/functions/position.ts
+++ src/functions/position.ts
@@ -2,6 +2,7 @@ import { BigInt } from '@graphprotocol/graph-ts'
 import { Position } from '../../generated/schema'
 
 export function getPosition(tokenId: BigInt): Position {
+  //todo: should prob update this to call the contract to get position details
   let position = Position.load(tokenId.toString())
   if (position == null) {
     position = new Position(tokenId.toString())
